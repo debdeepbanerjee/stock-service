138ddce66e3b2578b78df9a5de34057959015e9b
Debdeep <banerjee.debdeep@gmail.com>
2020-05-10 08:24:05 -0700

    first commit

---
+++ src/test/java/com/techprimers/stock/stockservice/StockServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package com.techprimers.stock.stockservice;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@Ignore
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StockServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
